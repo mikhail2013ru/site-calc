0e71a7ce9e9634abb0c1f20248fc80e6868f6a75
mikhail2013ru <mikhail2013ru@gmail.com>
2023-07-02 21:26:46 +0300

    2 день

--- js/view/radioPrograms.js
+++ js/view/radioPrograms.js
@@ -1,3 +1,5 @@
+import updateModel from "./../utils/updateModel.js"
+
 function init(getData) {
     const radioBtns = document.querySelectorAll('input[name="program"]')
     const { base, it, gov, zero } = getData().programs
@@ -13,16 +15,11 @@ function init(getData) {
 
     radioBtns.forEach(function (radioBtn) {
         radioBtn.addEventListener('change', function () {
-            console.log(this.value)
-
-            this.dispatchEvent(new CustomEvent('updateForm', {
-                bubbles: true,
-                detail: {
-                    selectedProgram: parseFloat(this.value),
-                    onUpdate: 'radioProgram',
-                    id: this.id
-                }
-            }))
+            updateModel(this, {
+                selectedProgram: parseFloat(this.value),
+                onUpdate: 'radioProgram',
+                id: this.id
+            })
         })
     })
 }
